a5723d2e1454cef258380786e54d4e5be26cacba
AKToran <aktoran60@gmail.com>
2026-05-07 21:57:15 +0600

    type guard or type narrowing

---
+++ src/typeGuard.ts
@@ -0,0 +1,42 @@
+// type narrowing
+//in, typeof
+
+type Alphanumeric = number | string;
+
+const add = (num1: Alphanumeric, num2: Alphanumeric) =>{
+  if(typeof num1 === 'number' && typeof num2 === 'number'){
+    return num1 + num2;
+  }
+  else {
+    return num1.toString() + num2.toString();
+  }
+}
+
+console.log(add(2, 2)); // 4
+
+console.log(add(2, "2")); // 22
+
+add("2", 2); // 22
+
+add("2", "2"); //22
+
+// in guard
+
+type NormalUser = {
+  name: string;
+};
+
+type AdminUser = {
+  name: string;
+  role: "Admin";
+};
+
+const getUserInfo = (user: NormalUser | AdminUser) => {
+  if ("role" in user) {
+    console.log(`${user.name} and his role is : ${user.role}`);
+  } else {
+    console.log(` ${user.name}`);
+  }
+};
+
+getUserInfo({ name: "Malik", role: "Admin" });
